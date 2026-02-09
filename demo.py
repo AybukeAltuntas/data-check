b70969c28b43b8108d190ea96dd038d9ded1b26f
Aybuke Altuntas <aybukealtuntas@192.168.1.19>
2026-02-09 16:31:09 +0300

    Add circle_area function

--- demo.py
+++ demo.py
@@ -2,4 +2,11 @@
 
 def circle_area(radius):
     """Returns the area of the circle of given radius"""
+    pi= 3.14
     pass  # YOUR CODE HERE
+    if radius <0:
+        return 0
+    A = pi * radius * radius
+    return A
+
+print(circle_area(4))
